25e0667a67768a09574cd98cb0a3f6f65c68b907
Benjamin Aplin <benjaminaplin@gmail.com>
2015-08-25 15:14:09 -0400

    db

--- song_sung/db/migrate/20150825185617_create_venue.rb
+++ song_sung/db/migrate/20150825185617_create_venue.rb
@@ -1,6 +1,10 @@
 class CreateVenue < ActiveRecord::Migration
   def change
     create_table :venues do |t|
+      t.string :name
+      t.city :string
+      t.state :string
+      t.family_friendly :boolean, default: false
     end
   end
 end
