434444caa047a0fd60fd68aa635220e4aac9e9d6
Benjamin Aplin <benjaminaplin@gmail.com>
2015-08-25 18:50:46 -0400

    tweakin

--- song_sung/app/controllers/search_controller.rb
+++ song_sung/app/controllers/search_controller.rb
@@ -1,8 +1,6 @@
 class SearchController < ApplicationController
   def search
     @events = Event.find_by_date(params[:date])
-    puts @events
-    binding.pry
     redirect_to event_path(@events)
   end
 end
